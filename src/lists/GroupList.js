7dcd36599c38518c6f93b058b33c97770e2b6cde
Krzysztof Surulo <krzysztof.surulo@gmail.com>
2018-05-19 22:53:09 +0100

    Add group details

--- src/lists/GroupList.js
+++ src/lists/GroupList.js
@@ -4,24 +4,67 @@ import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 
 class GroupList extends Component {
+    state = {
+        isDetails: false
+    };
+
     render() {
-        return (
-            <div>
-                <h3 style={{textAlign: 'center'}}>Groups</h3>
-                <List>
-                    <ListItem
-                        primaryText="Group 2"
-                        //rightIcon={<ActionGrade color={pinkA200}/>} favorites star?
-                        rightAvatar={<Avatar src={logo}/>}
-                    />
-                    <ListItem
-                        primaryText="Group 1"
-                        rightAvatar={<Avatar src={logo}/>}
-                    />
-                </List>
-            </div>
-        );
+
+        if (this.state.isDetails) {
+            return (
+                <div onClick={this.toggleGroupDetails}>
+                    <h3 style={{textAlign: 'center'}}>Groups</h3>
+                    <List>
+                        <ListItem
+                            primaryText="Group 2"
+                            onClick={this.toggleGroupDetails}
+                            //rightIcon={<ActionGrade color={pinkA200}/>} favorites star?
+                            rightAvatar={<Avatar src={logo}/>}
+                        />
+                        <ListItem
+                            primaryText="Group 1"
+                            onClick={this.toggleGroupDetails}
+                            rightAvatar={<Avatar src={logo}/>}
+                        />
+                    </List>
+                </div>);
+        }
+        else {
+            return (
+                <div onClick={this.toggleGroupDetails}>
+                    <h3 style={{textAlign: 'center'}}>Group Details</h3>
+
+                    <img src={logo} className={"center"}/>
+                    <h4 className={"center-text"}>Group Name</h4>
+                    <h4 className={"center-text"}>Members</h4>
+                    <p className={"center-text"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh
+                        augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque
+                        aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec</p>
+                    <List>
+                        <ListItem
+                            primaryText="Member 2"
+                            onClick={this.toggleGroupDetails}
+                            //rightIcon={<ActionGrade color={pinkA200}/>} favorites star?
+                            rightAvatar={<Avatar src={logo}/>}
+                        />
+                        <ListItem
+                            primaryText="Member 1"
+                            onClick={this.toggleGroupDetails}
+                            rightAvatar={<Avatar src={logo}/>}
+                        />
+                    </List>
+
+
+                </div>
+            );
+        }
     }
+
+    toggleGroupDetails = () => {
+        this.setState({
+            isDetails: false
+        });
+    };
 }
 
 export default GroupList;
