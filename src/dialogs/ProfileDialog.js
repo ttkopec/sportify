7630e9080fe24c6b93eaef011d560b3a4a40d41e
ttkopec <tomek.kopec1@gmail.com>
2018-05-20 14:52:16 +0200

    Add test to ensure no duplicate values in ReactSymbols

--- src/dialogs/ProfileDialog.js
+++ src/dialogs/ProfileDialog.js
@@ -1,9 +1,14 @@
+import {Divider, Paper, TextField} from "material-ui";
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import logo from './favicon.ico';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import '../App.css';
+
+const style = {
+    marginLeft: '2px',
+};
 
 class ProfileDialog extends Component {
     state = {
@@ -40,18 +45,29 @@ class ProfileDialog extends Component {
                     onRequestClose={this.handleClose}
                 >
                     <div className={"center"}>
-                        <img src={logo} className={"center"}/>
-                        <h4 className={"center-text"}>Tomasz Kopeć</h4>
-
-                        <p className={"center-text"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh
-                            augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque
-                            aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec
-                            venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui
-                            id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus
-                            leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget
-                            neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et
-                            mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at
-                            aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</p>
+                        <img src={logo} className={"center"} style={{marginBottom: '10px'}}/>
+                        <Paper zDepth={2}>
+                            <TextField defaultValue="Tomasz"
+                                       floatingLabelText="First name"
+                                       style={style}
+                                       underlineShow={false}/>
+                            <Divider/>
+                            <TextField defaultValue="Kopec"
+                                       floatingLabelText="Lastname"
+                                       style={style}
+                                       underlineShow={false}/>
+                            <Divider/>
+                            <TextField defaultValue="Group1, Group2, Group3"
+                                       floatingLabelText="Groups"
+                                       style={style}
+                                       underlineShow={false}/>
+                            <Divider/>
+                            <TextField defaultValue="tkopec@gmail.com"
+                                       floatingLabelText="Email address"
+                                       style={style}
+                                       underlineShow={false}/>
+                            <Divider/>
+                        </Paper>
                     </div>
                 </Dialog>
             </div>
